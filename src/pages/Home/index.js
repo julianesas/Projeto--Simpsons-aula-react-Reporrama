17b90d025eb5d1106a770b5e488f055e24de1d25
julianesas <julle_andrade@hotmail.com>
2021-06-23 21:47:38 -0300

    first-commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,15 @@
+import Menu from '../../components/Menu'
+import Banner from '../../components/Banner'
+
+
+const Home = ()=>{
+    return(
+        <>
+            <Menu />
+            <Banner />
+        </>
+
+    )
+}
+
+export default Home
